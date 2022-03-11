19cfa85bbc5629648559d4a6486f81198ea6a1cb
asrakin <asrakin@asu.edu>
2022-03-10 20:23:28 -0700

    final

--- requirement.sh
+++ requirement.sh
@@ -1,7 +1,9 @@
 ## run the command : conda create -n deepsteal python=3.6 
-## conda activate deepsteal
+## run the command: conda activate deepsteal
+## finally run the command: bash requirement.sh
 
 
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 
 
+pip install tqdm
