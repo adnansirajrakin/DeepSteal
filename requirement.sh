71a7d88a5473d1c4279eea7c67a0f26945085945
Adnan Siraj Rakin <asrakin@asu.edu>
2022-03-10 20:30:31 -0700

    Delete requirement.sh

--- requirement.sh
+++
@@ -1,9 +0,0 @@
-## run the command : conda create -n deepsteal python=3.6 
-## run the command: conda activate deepsteal
-## finally run the command: bash requirement.sh
-
-
-conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
-
-
-pip install tqdm
